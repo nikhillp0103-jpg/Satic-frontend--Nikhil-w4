3752e34554c72af0dc573a8075533eb60b81a7a1
nikhillp0103-jpg <nikhillp0103@gmail.com>
2026-01-21 21:37:33 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,3 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const navMenu = document.querySelector('.nav-menu');
