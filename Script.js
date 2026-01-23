498649c7c95e6d58c79b6795f662a117364c7d37
nikhillp0103-jpg <nikhillp0103@gmail.com>
2026-01-23 17:08:49 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,17 +1,6 @@
-const observers = document.querySelectorAll('.section, .service-card, .tcard, .why-item');
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
 
-const reveal = new IntersectionObserver((entries)=>{
-  entries.forEach(entry=>{
-    if(entry.isIntersecting){
-      entry.target.style.opacity = 1;
-      entry.target.style.transform = "translateY(0)";
-    }
-  });
-});
-
-observers.forEach(el=>{
-  el.style.opacity = 0;
-  el.style.transform = "translateY(20px)";
-  el.style.transition = ".6s ease";
-  reveal.observe(el);
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('show');
 });
