75c1e36aae804c719c6411f2c00f1c18dc4a3051
nikhillp0103-jpg <nikhillp0103@gmail.com>
2026-01-23 17:20:04 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
+const hamburger=document.getElementById("hamburger");
+const nav=document.getElementById("nav");
 
-hamburger.addEventListener('click', () => {
-  nav.classList.toggle('show');
+hamburger.onclick=()=>nav.classList.toggle("active");
+
+const reveals=document.querySelectorAll(".reveal");
+window.addEventListener("scroll",()=>{
+    reveals.forEach(e=>{
+        if(e.getBoundingClientRect().top < window.innerHeight-100){
+            e.classList.add("active");
+        }
+    });
 });
